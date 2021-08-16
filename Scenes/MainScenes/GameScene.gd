8478b40fc765a6ce59aca3fcf4ed6af2fbd691c3
shark711 <wallace.roets@gmail.com>
2021-08-16 12:32:08 +1000

    Part 6

--- Scenes/MainScenes/GameScene.gd
+++ Scenes/MainScenes/GameScene.gd
@@ -8,10 +8,15 @@ var build_tile
 var build_location
 var build_type
 
+var current_wave = 0
+var enemies_in_wave = 0
+
 func _ready():
 	map_node = get_node("Map1")
 	for button in get_tree().get_nodes_in_group("build_buttons"):
 		button.connect("pressed", self, "initiate_build_mode", [button.get_name()])
+	start_next_wave()
+
 
 func _process(delta):
 	# UI animation
@@ -22,6 +27,7 @@ func _process(delta):
 	
 	if build_mode:
 		update_tower_preview()
+
 
 func _unhandled_input(event):
 	if event.is_action_released("ui_cancel") and build_mode == true:
@@ -36,12 +42,37 @@ func _unhandled_input(event):
 		initiate_build_mode("Missile")
 
 
+##
+## Enemy Wave Functions
+##
+func start_next_wave():
+	var wave_data = retrieve_wave_data()
+	yield(get_tree().create_timer(0.2), "timeout") ## Padding between waves so they don't start instantly
+	spawn_enemies(wave_data)
+
+func retrieve_wave_data():
+	var wave_data = [["BlueTank", 0.7], ["BlueTank", 0.1]]
+	current_wave += 1
+	enemies_in_wave = wave_data.size()
+	return wave_data
+
+func spawn_enemies(wave_data):
+	for i in wave_data:
+		var new_enemy = load("res://Scenes/Enemies/" + i[0] + ".tscn").instance()
+		map_node.get_node("Path").add_child(new_enemy, true)
+		yield(get_tree().create_timer(i[1]), "timeout")
+
+
+##
+## Building Functions
+##
 func initiate_build_mode(tower_type):
 	if build_mode:
 		cancel_build_mode()
 	build_type = tower_type + "T1"
 	build_mode = true
 	get_node("UI").set_tower_preview(build_type, get_global_mouse_position())
+
 
 func update_tower_preview():
 	var mouse_position = get_global_mouse_position()
@@ -57,14 +88,16 @@ func update_tower_preview():
 		get_node("UI").update_tower_preview(tile_position, "adff4545")
 		build_valid = false;
 
+
 func cancel_build_mode():
 	build_mode = false
 	build_valid = false
 	get_node("UI/TowerPreview").free()
 
+
 func verify_and_build():
 	if build_valid:
-		##test to verify player has enough cash
+		## Test to verify player has enough cash
 		var new_tower = load("res://Scenes/Turrets/" + build_type + ".tscn").instance()
 		new_tower.position = build_location
 		map_node.get_node("Turrets").add_child(new_tower, true)
