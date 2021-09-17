71a83441364844ce918d278ffb32d4d4be2c334b
shark711 <wallace.roets@gmail.com>
2021-09-17 19:42:05 +1000

    Part 9 - Shooting Turrets

--- Scenes/Enemies/BlueTank.gd
+++ Scenes/Enemies/BlueTank.gd
@@ -1,6 +1,15 @@
 extends PathFollow2D
 
 var speed = 150
+var hp = 50
+
+onready var healh_bar = get_node("HealthBar")
+
+
+func _ready():
+	healh_bar.max_value = hp
+	healh_bar.value = hp
+	healh_bar.set_as_toplevel(true)
 
 func _physics_process(delta):
 	move(delta)
@@ -8,3 +17,15 @@ func _physics_process(delta):
 
 func move(delta):
 	set_offset(get_offset() + speed * delta)
+	healh_bar.set_position(position - Vector2(30, 30))
+
+
+func on_hit(damage):
+	hp -= damage
+	healh_bar.value = hp
+	if hp <= 0:
+		on_destroy()
+
+
+func on_destroy():
+	self.queue_free()
